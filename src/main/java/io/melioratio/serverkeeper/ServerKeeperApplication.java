f8c190da0fad05a4cce23ff8cfacf71e3a8cc54e
owuraku-zenas <awukuzenas@gmail.com>
2023-04-22 09:18:39 +0000

    First commit

---
+++ src/main/java/io/melioratio/serverkeeper/ServerKeeperApplication.java
@@ -0,0 +1,13 @@
+package io.melioratio.serverkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/melioratio/serverkeeper/ServerkeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package io.melioratio.serverkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerkeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
